a593ce573e08e5174ed3f96335f5d2e81bf62453
LKMNHL <gi5138244@gmail.com>
2025-03-11 22:57:14 +0900

    solve...........

--- backjoon/18185_ramen.py
+++ backjoon/18185_ramen.py
@@ -35,17 +35,42 @@ def time_over(N, arr):
 
 def solution(N, arr):
     ans=0
-
-    for i in range(N-2):
-        if arr[i+1]>arr[i+2]:
+    # 1개만 구매: 개 당 3원으로 가장 비쌈
+    # 2개 구매: 개 당 2.5원
+    # 3개 구매: 개 당 1원
+    # 처음부터 3개를 구매할 경우, 자신+1>자신+2일 때 최적이 아님
+    # 자신, 자신+1 먼저 소진하는 방식으로...
+    for i in range(len(arr)-2):
+        if arr[i+1]>arr[i+2]: # 나+1이 나+2보다 큰 경우 (2묶음 -> 3묶음 -> 1묶음 순)
             times=min(arr[i], arr[i+1]-arr[i+2])
             arr[i]-=times
             arr[i+1]-=times
             ans+=5*times
 
-            
+            remain_times=min(arr[i], arr[i+1], arr[i+2])
+            arr[i]-=remain_times
+            arr[i+1]-=remain_times
+            arr[i+2]-=remain_times
+            ans+=7*remain_times
 
+            ans += arr[i] * 3
+
+        else: # 나+2가 나+1보다 큰 경우 (3묶음 -> 2묶음 -> 1묶음 순)
+            times=min(arr[i], arr[i+1], arr[i+2])
+            arr[i]-=times
+            arr[i+1]-=times
+            arr[i+2]-=times
+            ans+=7*times
+
+            remain_times=min(arr[i], arr[i+1])
+            arr[i]-=remain_times
+            arr[i+1]-=remain_times
+            ans+=5*remain_times
+
+            ans += arr[i] * 3
+
+    return ans
 
 N=int(input())
-arr=list(map(int, input().split()))
+arr=list(map(int, input().split()))+[0, 0]
 print(solution(N, arr))
