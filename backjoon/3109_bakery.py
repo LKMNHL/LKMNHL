a04f89344ea5e44bd3a6f9aa30ccf645017d7620
LKM <diw6521@naver.com>
2025-03-12 09:04:36 +0900

    solving...

--- backjoon/3109_bakery.py
+++ backjoon/3109_bakery.py
@@ -1,41 +1,47 @@
 def dfs():
     global cnt
-    delta=[[0, 1], [-1, 1], [1, 1]] # 상, 상우, 하우
+    delta=[[-1, 1], [0, 1], [1, 1]] # 상우, 우, 하우
     while stack:
+        # print(visited)
         i, j=stack.pop()
 
         if j==M-2: # 종료조건
             cnt+=1 # 갈 수 있는 길 계수
             return
 
-        if visited[i][j]: # 방문했으면 탐색 중지
-            continue
-
         for di, dj in delta: # delta 방향으로 탐색
             ni, nj=i+di, j+dj
 
-            if 0<=ni<N and 0<=nj<M: # 유효범위일 때
+            if 0<=ni<N and 0<=nj<M and not visited[ni][nj]: # 유효 인덱스, 방문하지 않은 곳이면
                 if arr[ni][nj]=='.': # 통로일 때
                     stack.append([ni, nj])
                     visited[ni][nj]=1
+                    break
 
 
+'''
+5 5
+.xx..
+..x..
+.....
+...x.
+...x.
 
-
-
-
+'''
 
 N, M=list(map(int, input().split()))
-arr=[list(input().split()) for _ in range(N)]
+arr=[list(input().strip()) for _ in range(N)]
 
 stack=[]
 visited=[[0]*M for _ in range(N)]
 
 cnt=0
 
+
 for i in range(N):
-    for j in range(1, M-1):
-        if arr[i][j]=='.' and not visited[i][j]:
-            stack.append([i, j])
-            visited[i][j]+=1
-            dfs()
+    if arr[i][1]=='.' and not visited[i][1]:
+        stack.append([i, 1])
+        visited[i][1]=1
+        dfs()
+
+print(cnt)
