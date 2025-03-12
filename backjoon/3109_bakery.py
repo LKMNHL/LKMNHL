38f452bda649486baaffca64e1140c562ec8d841
LKMNHL <gi5138244@gmail.com>
2025-03-12 22:19:39 +0900

    solve...

--- backjoon/3109_bakery.py
+++ backjoon/3109_bakery.py
@@ -1,22 +1,19 @@
-def dfs():
+def dfs(i, j):
     global cnt
     delta=[[-1, 1], [0, 1], [1, 1]] # 상우, 우, 하우 (최대한 위쪽으로 붙어야 많은 파이프를 배치할 수 있음)
-    while stack:
-        # print(visited)
-        i, j=stack.pop()
 
-        if j==M-1: # 종료조건
-            cnt+=1 # 갈 수 있는 길 계수
-            return
+    if j == M - 1:  # 종료조건
+        cnt += 1  # 갈 수 있는 길 계수
+        return True
 
-        for di, dj in delta: # delta 방향으로 탐색
-            ni, nj=i+di, j+dj
+    for di, dj in delta:  # delta 방향으로 탐색
+        ni, nj = i + di, j + dj
 
-            if 0<=ni<N and 0<=nj<M and not visited[ni][nj]: # 유효 인덱스, 방문하지 않은 곳이면
-                if arr[ni][nj]=='.': # 통로일 때
-                    stack.append([ni, nj])
-                    visited[ni][nj]=1
-                    break
+        if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj]:  # 유효 인덱스, 방문하지 않은 곳이면
+            if arr[ni][nj] == '.':  # 통로일 때
+                visited[ni][nj] = 1
+                if dfs(ni, nj):
+                    return True
 
 '''
 5 5
@@ -41,6 +38,6 @@ for i in range(N):
     if arr[i][1]=='.' and not visited[i][1]:
         stack.append([i, 1])
         visited[i][1]=1
-        dfs()
+        dfs(i, 1)
 
 print(cnt)
